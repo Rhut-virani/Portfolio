57d0232091fab2caed0f9bc056704a1e7be2ce64
Rhut Virani <virani.rhut@gmail.com>
2026-01-05 15:28:01 -0700

    update browserlist

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,29 @@
 module.exports = function (grunt) {
-	grunt.initConfig({
-		postcss: {
-			options: {
-				map: true, // inline sourcemaps
+    grunt.initConfig({
+        postcss: {
+            options: {
+                map: true, // inline sourcemaps
+                map: {
+                    inline: false, // save all sourcemaps as separate files...
+                    annotation: 'dist/css/maps/', // ...to the specified directory
+                },
+                processors: [
+                    require('pixrem')(), // add fallbacks for rem units
+                    require('autoprefixer')(), // add vendor prefixes
+                    require('cssnano')(), // minify the result
+                ],
+            },
+            dist: {
+                src: 'style.css',
+                dest: 'build/style.css',
+            },
+        },
+    });
 
-				// or
-				map: {
-					inline: false, // save all sourcemaps as separate files...
-					annotation: "dist/css/maps/", // ...to the specified directory
-				},
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-				processors: [
-					require("pixrem")(), // add fallbacks for rem units
-					require("autoprefixer")({ browsers: "last 5 versions" }), // add vendor prefixes
-					require("cssnano")(), // minify the result
-				],
-			},
-			dist: {
-				src: "style.css",
-				dest: "build/style.css",
-			},
-		},
-	});
-	grunt.loadNpmTasks("grunt-postcss");
-	grunt.loadNpmTasks("grunt-contrib-watch");
+    // Register tasks
+    grunt.registerTask('build', ['postcss']);
+    grunt.registerTask('default', ['watch']);
 };
-
-// grunt postcss
